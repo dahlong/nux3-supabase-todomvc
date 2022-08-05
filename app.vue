6905bc188f0851d6ccbad80562dc542ef03310ae
dahlong <dahlong@nchc.narl.org.tw>
2022-08-05 16:35:05 +0800

    config: init env variable

--- app.vue
+++ app.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
     <h1>Nuxt 3 Superbase ToDoMVC</h1>
+    <h2>{{supabaseUrl}}</h2>
+    <h2>{{supabaseKey}}</h2>
+    
     <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<script setup>
+
+import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = 'https://rndcngutekzsxiyrralu.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
+
+const config = useRuntimeConfig()
+console.log(config)
+
+const supabaseUrl = config.supabaseUrl
+const supabaseKey = config.supabaseKey
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    publicRuntimeConfig: {
+        supabaseUrl: process.env.SUPERBASE_URL,
+        supabaseKey: process.env.SUPERBASE_KEY,
+    }
 })
